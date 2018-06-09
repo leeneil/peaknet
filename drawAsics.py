87cbdca79f7f28f04b0e469d439c2fd7659c31d4
leeneil <lishantreasure@gmail.com>
2018-06-09 01:27:09 -0700

    fixed bugs in drawing

--- drawAsics.py
+++ drawAsics.py
@@ -1,64 +1,83 @@
 import sys
 import re
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import matplotlib.patches as pat
 import matplotlib.lines as mlines
+from matplotlib.offsetbox import (TextArea, DrawingArea, OffsetImage,
+                                  AnnotationBbox)
+
 
 outputPath = "output/"
 
-filename = sys.argv[1]
-print(filename)
+filename1 = sys.argv[1]
+print(filename1)
 
-txt = open(filename, 'r').readlines()
+filename2 = sys.argv[2]
+print(filename2)
+
+# img = plt.imread(filename2)
+img = np.load(filename2)
+print('image shape', img.shape)
+
+txt = open(filename1, 'r').readlines()
 
 data = []
 for i, line in enumerate(txt):
     if "/min_fs" in line:
         det = {}
-        print(line)
+        # print(line)
         vals = line.split(' = ')
         det["min_fs"] = int( vals[1].rstrip() )
     if "/min_ss" in line:
-        print(line)
+        # print(line)
         vals = line.split(' = ')
         det["min_ss"] = int( vals[1].rstrip() )
     if "/max_fs" in line:
-        print(line)
+        # print(line)
         vals = line.split(' = ')
         det["max_fs"] = int( vals[1].rstrip() )
     if "/max_ss" in line:
-        print(line)
+        # print(line)
         vals = line.split(' = ')
         det["max_ss"] = int( vals[1].rstrip() )
     if "/fs" in line:
-        print(line)
+        # print(line)
         result = re.split(r'([+\/-][\d\.]+)', line)
         det["fs"] = ( round(float(result[1])), round(float(result[3])) )
     if "/ss" in line:
-        print(line)
+        # print(line)
         result = re.split(r'([+\/-][\d\.]+)', line)
         det["ss"] = ( round(float(result[1])), round(float(result[3])) )
     if "/corner_x" in line:
-        print(line)
+        # print(line)
         vals = line.split(' = ')
         det["corner_x"] = float( vals[1].rstrip() )
     if "/corner_y" in line:
-        print(line)
+        # print(line)
         vals = line.split(' = ')
         det["corner_y"] = float( vals[1].rstrip() )
     if "/coffset" in line:
-        print(line)
+        # print(line)
         vals = line.split(' = ')
         det["coffset"] = float( vals[1].rstrip() )
         data.append( det )
-print(data)
+# print(data)
 
 print('# of ASICs:', len(data))
 
 box_ss = 185
 box_fs = 194
 
+x1 = -1748/2.0 + 5 - 2
+y1 = -1739/2.0 - 9 + 1
+x2 = x1 + img.shape[1] - 1
+y2 = y1 + img.shape[0] - 1
+
 fig, ax = plt.subplots(1)
+im0 = plt.imshow(np.flipud(np.rot90(img,3)), vmin=0, vmax=2550, cmap=cm.gray,
+                extent=[x1, x2, y1, y2])
 for det in data:
     if det["fs"][1] == 1:
         y_cor = det["corner_y"]
@@ -85,7 +104,7 @@ for det in data:
         x_cor = det["corner_x"]-box_fs
         box_x = box_fs
 
-    plt.plot( det["corner_x"], det["corner_y"], 'ks', 'Size', 0.5 )
+    # plt.plot( det["corner_x"], det["corner_y"], 'ks' )
     rect = pat.Rectangle( (x_cor, y_cor),
         box_x, box_y, color="c", fill=False, linewidth=0.5)
     ax.add_patch(rect)
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import json
 from math import sqrt
+import re
+import numpy as np
 
 w = 1552
 h = 1480
@@ -29,6 +31,137 @@ def loadResults(filename):
             data[pid] = []
         data[pid].append( {'bbox':peak['bbox'], 'score':peak['score']} )
     return data
+
+def readStream(filename):
+    txt = open(filename, 'r').readlines()
+    data = []
+    for i, line in enumerate(txt):
+        if "/min_fs" in line:
+            det = {}
+            # print(line)
+            vals = line.split(' = ')
+            det["min_fs"] = int( vals[1].rstrip() )
+        if "/min_ss" in line:
+            # print(line)
+            vals = line.split(' = ')
+            det["min_ss"] = int( vals[1].rstrip() )
+        if "/max_fs" in line:
+            # print(line)
+            vals = line.split(' = ')
+            det["max_fs"] = int( vals[1].rstrip() )
+        if "/max_ss" in line:
+            # print(line)
+            vals = line.split(' = ')
+            det["max_ss"] = int( vals[1].rstrip() )
+        if "/fs" in line:
+            # print(line)
+            result = re.split(r'([+\/-][\d\.]+)', line)
+            det["fs"] = ( round(float(result[1])), round(float(result[3])) )
+        if "/ss" in line:
+            # print(line)
+            result = re.split(r'([+\/-][\d\.]+)', line)
+            det["ss"] = ( round(float(result[1])), round(float(result[3])) )
+        if "/corner_x" in line:
+            # print(line)
+            vals = line.split(' = ')
+            det["corner_x"] = float( vals[1].rstrip() )
+        if "/corner_y" in line:
+            # print(line)
+            vals = line.split(' = ')
+            det["corner_y"] = float( vals[1].rstrip() )
+        if "/coffset" in line:
+            # print(line)
+            vals = line.split(' = ')
+            det["coffset"] = float( vals[1].rstrip() )
+            data.append( det )
+    return data
+
+def cheetah2Det( stream_data, xys, thresh=0.1 ):
+    # test format of xys
+    if type(xys[0]) is tuple:
+        xys2 = xys.copy()
+    else:
+        xys2 = []
+        for xy in xys:
+            if xy["score"] > thresh:
+                xys2.append(
+                    (xy["bbox"][0]+0.5*xy["bbox"][2], xy["bbox"][1]+0.5*xy["bbox"][3] ) )
+    new_xys = []
+    for x, y in xys2:
+        for asic in stream_data:
+            if x >= asic["min_fs"] and x <= asic["max_fs"]:
+                pass
+            else:
+                continue
+            if y >= asic["min_ss"] and y <= asic["max_ss"]:
+                pass
+            else:
+                continue
+            x = x % 194
+            y = y % 185
+            # xp = asic["corner_x"]
+            # yp = asic["corner_y"]
+            if asic["fs"][0] == 1:
+                xp = x + asic["corner_x"]
+            elif asic["fs"][0] == -1:
+                xp = -x + asic["corner_x"]
+            elif asic["ss"][0] == 1:
+                xp = y + asic["corner_x"]
+            elif asic["ss"][0] == -1:
+                xp = -y + asic["corner_x"]
+            if asic["fs"][1] == 1:
+                yp = x + asic["corner_y"]
+            elif asic["fs"][1] == -1:
+                yp = -x + asic["corner_y"]
+            elif asic["ss"][1] == 1:
+                yp = y + asic["corner_y"]
+            elif asic["ss"][1] == -1:
+                yp = -y + asic["corner_y"]
+            new_xys.append( (xp+0.5, yp+0.5) )
+            break
+    return new_xys
+
+def drawAsic( stream_data, img ):
+    asics = []
+    ss_len = 185
+    fs_len = 194
+    for asic in stream_data:
+        my_img = np.flipud(
+            img[asic["min_ss"]:(asic["max_ss"]+1),asic["min_fs"]:(asic["max_fs"]+1)],
+            )
+        if asic["fs"][0] == 1:
+            x1 = asic["corner_x"]
+            x2 = x1 + fs_len
+            t = 0
+        elif asic["fs"][0] == -1:
+            x2 = asic["corner_x"]
+            x1 = x2 - fs_len
+            t = 2
+        elif asic["ss"][0] == 1:
+            x1 = asic["corner_x"]
+            x2 = x1 + ss_len
+            t = 3
+        elif asic["ss"][0] == -1:
+            x2 = asic["corner_x"]
+            x1 = x2 - ss_len
+            t = 1
+        if asic["fs"][1] == 1:
+            y1 = asic["corner_y"]
+            y2 = y1 + fs_len
+        elif asic["fs"][1] == -1:
+            y2 = asic["corner_y"]
+            y1 = y2 - fs_len
+        elif asic["ss"][1] == 1:
+            y1 = asic["corner_y"]
+            y2 = y1 + ss_len
+        elif asic["ss"][1] == -1:
+            y2 = asic["corner_y"]
+            y1 = y2 - ss_len
+        asics.append( {"img":np.rot90(my_img,t), "extend":(x1,x2,y1,y2)} )
+    return asics
+
+
+
 
 def IOU( x1, y1, w1, h1, x2, y2, w2, h2):
     box1 = ( x1-w1/2.0, y1-h1/2.0, x1+w1/2.0, y1+h1/2.0)
